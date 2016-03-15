351608c6ead60b58c2faf7836b425e12f82ec2a8
WuHui <wuhui_0930@163.com>
2016-03-15 20:24:39 +0800

    first

---
+++ app/src/main/java/crop/ui/qcgj/android/tulip/com/imagecrop/util/Log.java
@@ -0,0 +1,15 @@
+package crop.ui.qcgj.android.tulip.com.imagecrop.util;
+
+public class Log {
+
+    private static final String TAG = "android-crop";
+
+    public static final void e(String msg) {
+        android.util.Log.e(TAG, msg);
+    }
+
+    public static final void e(String msg, Throwable e) {
+        android.util.Log.e(TAG, msg, e);
+    }
+
+}
---
+++ app/src/main/java/crop/ui/qcgj/android/tulip/com/imagecrop/util/VisibleForTesting.java
@@ -0,0 +1,12 @@
+package crop.ui.qcgj.android.tulip.com.imagecrop.util;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.METHOD, ElementType.TYPE})
+public @interface VisibleForTesting {}
